7b92cf0df40424d9fee88635d1fdaf61efd07802
Daniel Arevalo Rodrigo <sirl.daniel.arevalo@hotmail.com>
2019-11-20 12:06:03 +0100

    sync

--- src/main/java/urjc/isi/pruebasSparkJava2/Main.java
+++ src/main/java/urjc/isi/pruebasSparkJava2/Main.java
@@ -18,6 +18,7 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         port(getHerokuAssignedPort());
 
+        
         // spark server
         get("/hello", Main::doWork);
 
